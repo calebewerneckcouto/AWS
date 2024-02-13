9e084ce55e6c7078054ef9631e463acb8f57c025
Calebe <calebewerneck@hotmail.com>
2024-02-13 15:26:34 -0300

    primeiro commit

---
+++ src/test/java/br/com/apiproduto/AwsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apiproduto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
